bcc731ff1ea07901de84f864e13449fcf6b0f52c
snaimeh-alhisba <101411167+snaimeh-alhisba@users.noreply.github.com>
2022-07-29 15:39:14 +0300

    add auth packege and setup model and app

--- config/app.js
+++ config/app.js
@@ -4,11 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//module for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportLocal = require('passport-local');
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect-flash');
+
 var indexRouter = require('../app/routes');
 let accountRouter = require('../app/routes/account');
 let surveyRouter = require('../app/routes/survey');
-
-var app = express();
 
 // database setup
 let mongoose = require('mongoose');
@@ -24,6 +29,8 @@ mongoDB.once('open', ()=>{
   console.log('Connected to MongoDB...');
 });
 
+var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, '../app/views'));
 app.set('view engine', 'ejs');
@@ -34,6 +41,31 @@ app.use(express.urlencoded({ extended: true })); // default false, updated by ST
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../node_modules')));
+
+//setup express session
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: false,
+  resave: false
+}));
+
+//initialize flash for messages
+app.use(flash());
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//create a user model instance for the passport user configuration
+let userModel = require('../app/models/user');
+let User = userModel.User;
+
+//user strategy for authentication login
+passport.use(User.createStrategy());
+
+//encrypt and decrypt user information
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use('/', indexRouter);
 app.use('/account', accountRouter);
